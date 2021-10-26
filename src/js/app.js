1247e3482618ecc989b8ee8ec029eb20e78cbbbc
kristina <kistina@localhost>
2021-10-26 16:24:09 +0300

    update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
 import Chaos from './Chaos';
 
-const chaos = new Chaos(document.querySelector('.chaos_organizer'), 'https://ahj-diplom-back.herokuapp.com/');
+const chaos = new Chaos(document.querySelector('.chaos_organizer'), 'https://ahj-diplom-back.herokuapp.com');
 chaos.init();
